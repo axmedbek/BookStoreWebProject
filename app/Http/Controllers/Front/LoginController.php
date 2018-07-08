5c9e9e75c5f2452e6b785d51ef8bf0e64281f258
axmedbek <axmed.memmedli.96@mail.ru>
2018-07-08 23:09:42 +0400

    "Login" was changed v1

--- app/Http/Controllers/Front/LoginController.php
+++ app/Http/Controllers/Front/LoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -17,13 +18,24 @@ class LoginController extends Controller
 
     public function loginProcess(Request $request)
     {
-        if (Auth::attempt(['email' => $request->get('email') , 'password' => $request->get('password')])
-        || Auth::attempt(['username' => $request->get('email') , 'password' => $request->get('password')])) {
-            request()->session()->regenerate();
-            return redirect()->intended('/');
-        } else {
-            $errors = ['email' => 'Girisde xeta var'];
-            return back()->withErrors($errors);
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|string|email|exists:users,email',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validator->fails()){
+
+            return back()->withErrors($validator->errors());
+        }
+        else{
+            if (Auth::attempt(['email' => $request->get('email') , 'password' => $request->get('password')])
+                || Auth::attempt(['username' => $request->get('email') , 'password' => $request->get('password')])) {
+                request()->session()->regenerate();
+                return redirect()->intended('/');
+            } else {
+                $errors = ['email' => 'Girisde xeta var'];
+                return back()->withErrors($errors);
+            }
         }
 
     }
